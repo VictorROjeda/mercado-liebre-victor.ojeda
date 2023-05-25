71108a5164adc9a692c7c1f30879c3233f159c8a
VictorROjeda <victor.r.ojeda@gmail.com>
2023-05-24 21:28:53 -0300

    subida de archivos

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, "./public");
+
+app.use(express.static(publicPath));
+
+app.listen(process.env.PORT || 3000, () => console.log("Servidor corriendo"));
+
+
+//app.get("/", function(req, res){
+//    res.send("Hola Gente!")
+//});
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "./views/home.html"))
+});
+
+
+
+
