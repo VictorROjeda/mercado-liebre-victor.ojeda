feb779cc8d597592d11ca7b9bef838aab456777a
VictorROjeda <victor.r.ojeda@gmail.com>
2023-05-26 15:52:34 -0300

    agregando modificaciones - incorporación de formularios login y register

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const publicPath = path.resolve(__dirname, "./public");
 
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT || 3000, () => console.log("Servidor corriendo"));
+app.listen(3000, () => console.log("Servidor corriendo"));
 
 
 //app.get("/", function(req, res){
@@ -19,5 +19,11 @@ app.get("/", function(req, res){
 });
 
 
+app.get("/login", function(req, res){
+    res.sendFile(path.join(__dirname, "./views/login.html"))
+});
 
+app.get("/register", function(req, res){
+    res.sendFile(path.join(__dirname, "./views/register.html"))
+});
 
